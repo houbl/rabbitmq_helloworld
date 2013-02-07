7afc9bb1bbdf5d58f21b41fc8a59c6659766dc15
Mike Chester <m.chester@hotmail.com>
2013-02-07 15:26:12 -0800

    Now sends/receives JSON

--- receive.py
+++ receive.py
@@ -1,19 +1,32 @@
-#!/usr/bin/env_python
+#!/usr/bin/env python
 import pika
+import json
+import os
+import time
+
+# RabbitMQ Server
+host = 'elephant109.heprc.uvic.ca'
+queue = 'squiddata'
+json_folder = '/home/mchester/rabbitmq_helloworld/dumps'
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-	host='localhost'))
+	host,5672))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue=queue)
 
 print ' [*] Waiting for messages. To exit press CTRL+C'
 
 def callback(ch, method, properties, body):
-	print " [x] Received %r" % (body,)
+    data = json.loads(body)
+    file = '{0}.json'.format(os.path.join(json_folder,data['hostname']))
+
+    print " [x] Received {0}.".format(data['hostname'])
+    print data
+
 
 channel.basic_consume(callback,
-		      queue='hello',
+		      queue=queue,
 		      no_ack=True)
 
 channel.start_consuming()
--- send.py
+++ send.py
@@ -1,18 +1,45 @@
-#!/usr/bin/env_python
+#!/usr/bin/env python
 import pika
 import sys
+import json
+import socket
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-	host='localhost'))
-channel = connection.channel()
+# RabbitMQ Server
+broker = 'elephant109.heprc.uvic.ca'
+queue = 'squiddata'
 
-channel.queue_declare(queue='hello')
+def amqp_send(data):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+                    broker, 5672))
+    channel = connection.channel()
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
+    channel.queue_declare(queue=queue)
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body=message)
-print " [x] Sent %r" % (message,)
-connection.close()
+    channel.basic_publish(exchange='',
+                          routing_key=queue,
+                          body=data)
+    print " [x] Sent ",data
+    connection.close()
 
+data = {}
+data['hostname'] = socket.gethostname()
+data['key2'] = "value"
+
+json_str = json.dumps(data)
+
+amqp_send(json_str)
+
+
+
+
+
+
+
+#result = channel.exchange_declare(exchange='logs',
+#				  type='fanout')
+#
+#
+#channel.basic_publish(exchange='logs',
+#                      routing_key='',
+#                      body=message)
+#print " [x] Sent %r" % (message,)
